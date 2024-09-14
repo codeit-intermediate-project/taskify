e10fb488a7d78e8fcb56062c9e78a185999984b3
Minjo Kwawk <iclock1960@naver.com>
2024-09-14 11:19:46 +0900

    [Feat] 모달창 반응형 디자인 완료

--- src/components/Auth/AuthModal.tsx
+++ src/components/Auth/AuthModal.tsx
@@ -8,11 +8,13 @@ interface ModalProps {
 export default function Modal({ message, onClose }: ModalProps) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-[#000000] bg-opacity-70">
-      <div className="flex h-[220px] w-[327px] flex-col items-center gap-[50px] rounded-lg bg-white p-7">
-        <p className="mt-9 text-black-600 font-lg-16px-medium">{message}</p>
+      <div className="flex h-[220px] w-[327px] flex-col items-center gap-[50px] rounded-2xl bg-white p-7 md:h-[192px] md:w-[368px] md:justify-center md:gap-[32px]">
+        <p className="mt-9 text-black-600 font-lg-16px-medium md:mt-0 md:font-xl-20px-medium">
+          {message}
+        </p>
         <button
           onClick={onClose}
-          className="flex h-[42px] w-[138px] items-center justify-center rounded-lg bg-violet text-white font-md-14px-regular"
+          className="flex h-[42px] w-[138px] items-center justify-center rounded-lg bg-violet text-white font-md-14px-regular md:h-[48px] md:w-[240px]"
         >
           닫기
         </button>
