402d84eab7d8440bf5c373f725957474a8723cac
kimsayhi <kimse418@gmail.com>
2024-09-15 16:49:04 +0900

    [Refactor]: 플로팅 버튼이 처음에 첫번째 컬럼에 focus되도록 수정

--- src/components/dashboard/UI/FloatingColumnList.tsx
+++ src/components/dashboard/UI/FloatingColumnList.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef } from 'react';
+import { useContext, useRef } from 'react';
 
 import { DashBoardContext } from '@core/contexts/DashBoardContext';
 
@@ -12,15 +12,7 @@ export default function FloatingColumnList({
 }: FloatingColumnListProps) {
   const { columnList } = useContext(DashBoardContext);
   const buttonRefs = useRef<HTMLButtonElement[] | null[]>([]);
-  useEffect(() => {
-    buttonRefs.current.forEach((button, index) => {
-      if (button && index === focusIndex) {
-        button.classList.add('focused-column');
-      } else {
-        button?.classList.remove('focused-column');
-      }
-    });
-  }, [focusIndex]);
+
   return (
     <div className="absolute right-10 top-[300px] hidden flex-col items-end gap-2 text-gray-300 xl:flex">
       {columnList.map((column, index) => (
@@ -32,7 +24,7 @@ export default function FloatingColumnList({
           onClick={() => {
             onClickMoveFloatingButton(index);
           }}
-          className={`${index === focusIndex ? 'focused-column' : ''}pointer hover:column-hover flex items-center gap-1 rounded-[20px] transition-all duration-300 ease-in-out`}
+          className={`${index === focusIndex ? 'focused-column' : ''} pointer hover:column-hover flex items-center gap-1 rounded-[20px] transition-all duration-300 ease-in-out`}
         >
           <span className="pr-1font-lg-16px-semibold">{column.title}</span>
         </button>
