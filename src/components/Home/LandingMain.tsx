8caff925a2489411ab607af34344013109ce89ab
Minjo Kwawk <iclock1960@naver.com>
2024-09-17 22:54:47 +0900

    [Feat] react-intersection-observer 패키지 설치

--- src/components/Home/LandingMain.tsx
+++ src/components/Home/LandingMain.tsx
@@ -1,23 +1,41 @@
+import { motion } from 'framer-motion';
+
 import DescriptionCard from '@components/Home/DescriptionCard';
 import PointCard from '@components/Home/PointCard';
 
 export default function LandingMain() {
   return (
     <>
-      <div className="flex w-full flex-col items-center justify-center p-[16px]">
+      <motion.div
+        className="flex w-full flex-col items-center justify-center p-[16px]"
+        initial={{
+          opacity: 0,
+          y: -30,
+          x: 0,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          x: 0,
+        }}
+        transition={{
+          ease: 'easeIn',
+          duration: 0.6,
+        }}
+      >
         <PointCard type="01" />
         <PointCard type="02" />
-      </div>
-      <div className="mb-[120px] flex w-full flex-col items-center justify-center p-[16px]">
+      </motion.div>
+      <motion.div className="mb-[120px] flex w-full flex-col items-center justify-center p-[16px]">
         <p className="mb-[42px] mt-[31px] font-2xl-22px-regular md:font-2xl-28px-bold">
           생산성을 높이는 다양한 설정⚡
         </p>
-        <div className="xl:flex xl:flex-row xl:gap-8">
+        <motion.div className="xl:flex xl:flex-row xl:gap-8">
           <DescriptionCard card="01" />
           <DescriptionCard card="02" />
           <DescriptionCard card="03" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </>
   );
 }
